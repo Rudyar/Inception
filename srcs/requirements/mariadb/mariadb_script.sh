52c42610ad45c201ecdeef5deec0c9a5f01a4b9f
arudy <arudy@student.42.fr>
2022-10-20 18:30:42 +0200

    fix nginx pb with wordpress + add secure wp dld

--- srcs/requirements/mariadb/mariadb_script.sh
+++ srcs/requirements/mariadb/mariadb_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d "/var/lib/mysql/$DB_NAME"]; then
+if [ -d "/var/lib/mysql/$DB_NAME" ]; then
 	echo "DB is already configured"
 else
 	echo "Start DB configuration"
@@ -11,7 +11,6 @@ else
 # Create db, give privileges to root across all databases on the server from localhost,
 # Create a user and give privileges on DB_NAME from any host (remote, local...)
 # Flush privileges to update modif
-# Need to add root % too ???????
 	cat << EOF > file
 CREATE DATABASE $DB_NAME;
 FLUSH PRIVILEGES;
@@ -24,11 +23,7 @@ FLUSH PRIVILEGES;
 EOF
 
 # --bootstrap to execute sql script before any privileges exits
-echo "0==============\n"
 	mysqld --bootstrap < file 2> /dev/null
-echo "1==============\n"
 	rm file
 fi
-echo "2==============\n"
 mysqld 2> /dev/null
-echo "3==============\n"
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-wp core download --allow-root
-
 while ! mariadb -h$DB_HOST -u$DB_ADMIN -p$DB_ADMIN_PASSWORD; do
 	echo "Waiting to connect to MariaDB"
 	sleep 2
 done
 
-wp config create --dbname="$DB_NAME" --dbuser="$DB_ADMIN" \
-	--dbpass="$DB_ADMIN_PASSWORD" --dbhost="$DB_HOST" --allow-root
+if [ ! -f wp-config.php ]; then
+	wp core download --allow-root
+	wp config create --dbname="$DB_NAME" --dbuser="$DB_ADMIN" \
+		--dbpass="$DB_ADMIN_PASSWORD" --dbhost="$DB_HOST" --allow-root
 
-wp core install --url="$DOMAIN_NAME" --title="arudy inception" \
-	--admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD"\
-	--admin_email="$WP_ADMIN_MAIL" --skip-email --allow-root
+	wp core install --url="$DOMAIN_NAME" --title="arudy inception" \
+		--admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD"\
+		--admin_email="$WP_ADMIN_MAIL" --skip-email --allow-root
 
-wp user create $WP_USER $WP_MAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+	wp user create $WP_USER $WP_MAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+	echo "Wordpress installed and configured with a user"
+else
+	echo "Wordpress is already configured"
+fi
 
-echo "Wordpress installed and configured with a user"
 echo "\nVisit https://arudy.42.fr"
 
 # Launch wp
-php-fpm7.3 -F -R
+php-fpm7.3 -F
 
 # show user db wp : conect to mariadb container, mysql with db_admin
 # show databases, use wordpress, show tables, show columns from wp_users
